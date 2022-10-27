84d4c25b41295275fbc8dbe855316cb36446534d
Steven Bryant <stevelad_83@yahoo.com>
2022-10-27 18:38:24 -0400

    adding reset functionality

--- app.js
+++ app.js
@@ -23,15 +23,19 @@ const responsesArray = [
 const submitBtn = document.getElementById('submit');
 const answer = document.getElementById('renderAnswer');
 const resetBtn = document.getElementById('reset');
+const promptEl = document.getElementById('prompt');
+const replyEl = document.getElementById('reply');
+
+function toggleSections() {
+    promptEl.classList.toggle('hide');
+    replyEl.classList.toggle('hide');
+}
 
 submitBtn.addEventListener('click', () => {
+    toggleSections();
     const randomNum = Math.floor(Math.random() * responsesArray.length);
     const randomAnswer = responsesArray[randomNum];
     answer.textContent = randomAnswer;
 });
 
-function toggleSections() {}
-
-resetBtn.addEventListener('click', () => {
-    console.log('clicking the reset button!');
-});
+resetBtn.addEventListener('click', toggleSections);
