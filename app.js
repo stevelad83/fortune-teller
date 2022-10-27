a0d33429532fe4d119560bedd6cc69ff6eb57fbb
Steven Bryant <stevelad_83@yahoo.com>
2022-10-27 17:49:19 -0400

    adding functionality for generating random answer

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
+const responsesArray = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
+
 const submitBtn = document.getElementById('submit');
 
 submitBtn.addEventListener('click', () => {
-    console.log('Clicking the button!');
+    const randomNum = Math.floor(Math.random() * responsesArray.length);
+    const randomAnswer = responsesArray[randomNum];
+    console.log(randomAnswer);
 });
